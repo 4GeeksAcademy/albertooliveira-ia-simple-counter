ca9c10679f791c275d6b2fa5a50905f185c36197
Alberto Oliveira <albertooliveirajaen@gmail.com>
2026-04-19 13:24:12 +0000

    first commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+    const [seconds, setSeconds] = useState(0);
+    const [active, setActive] = useState(true);
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    useEffect(() => {
+        let interval = null;
+
+        if (active) {
+            interval = setInterval(() => {
+                setSeconds(prevSeconds => prevSeconds + 1);
+            }, 1000);
+        } else {
+            clearInterval(interval);
+        }
+
+        return () => clearInterval(interval);
+    }, [active]);
+
+    const handleAlert = () => {
+        let userTime = prompt("¿En qué segundo quieres la alerta?");
+    };
+
+    let unit = seconds % 10;
+    let tens = Math.floor(seconds / 10) % 10;
+    let hundreds = Math.floor(seconds / 100) % 10;
+    let thousands = Math.floor(seconds / 1000) % 10;
+    let tenThousands = Math.floor(seconds / 10000) % 10;
+    let hundredThousands = Math.floor(seconds / 100000) % 10;
+
+    return (
+        <div className="text-center mt-5">
+            <div className="d-flex justify-content-center bg-black text-white p-3 rounded mx-auto" style={{width: "fit-content"}}>
+                <div className="bg-dark m-1 p-3 rounded display-3"><i className="far fa-clock"></i></div>
+                <div className="bg-dark m-1 p-3 rounded display-3">{hundredThousands}</div>
+                <div className="bg-dark m-1 p-3 rounded display-3">{tenThousands}</div>
+                <div className="bg-dark m-1 p-3 rounded display-3">{thousands}</div>
+                <div className="bg-dark m-1 p-3 rounded display-3">{hundreds}</div>
+                <div className="bg-dark m-1 p-3 rounded display-3">{tens}</div>
+                <div className="bg-dark m-1 p-3 rounded display-3">{unit}</div>
+            </div>
+
+            <div className="mt-4">
+                <button className="btn btn-danger mx-1" onClick={() => setActive(false)}>Parar</button>
+                <button className="btn btn-success mx-1" onClick={() => setActive(true)}>Resumir</button>
+                <button className="btn btn-warning mx-1" onClick={() => setSeconds(0)}>Reiniciar</button>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
